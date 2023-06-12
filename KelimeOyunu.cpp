a668a543dd7c3db7648730600cab9541608caa4e
İclal Çengel <115634785+iclalcengel@users.noreply.github.com>
2023-06-13 00:34:46 +0300

    Update KelimeOyunu.cpp

--- KelimeOyunu.cpp
+++ KelimeOyunu.cpp
@@ -176,3 +176,92 @@ void KelimeOyunu::ZamaniDurdur(clock_t& before1, clock_t& after1) {
 	} while (tahmin.UpdateZaman(before, after));
 	ZamaniDurdurUtil(before, after, before1, after1, false);
 }
+// "ZamaniDurdur" fonksiyonu, yanıtı görüntüler, oyuncunun puanını günceller ve bir sonraki soruyu alır
+void KelimeOyunu::ZamaniDurdurUtil(clock_t& before, clock_t& after, clock_t& before1, clock_t& after1, const bool& guessed_correct) {
+	CevabiGoster();
+	ImlecTasiXY(46, 14);
+	std::cout << std::string(std::to_string(score).length(), ' ');
+	ImlecTasiXY(46, 14);
+	std::cout << (score += (guessed_correct ? 1 : -1) * no_of_undisplayed_letters * HARF_BASI_PUAN);
+	Sleep(1000);
+	before = after;
+	after = clock();
+	tahmin.UpdateZaman(before, after);
+	short dakika = TAHMIN_SURESI / 60 - tahmin.GetDakika();
+	double saniye = double(TAHMIN_SURESI % 60) - tahmin.GetSaniye();
+	if (saniye < 0.0) {
+		saniye += 60.0;
+		dakika--;
+	}
+	current_time.IncreaseZaman(dakika, saniye);
+	before1 = after1;
+	after1 = clock();
+	current_time.UpdateZaman(before1, after1);
+	GetNextQuestion();
+}
+
+// oyunun nasıl oynanacağına dair kuralları ve talimatları verir
+void KelimeOyunu::PrintOpeningScreen() {
+	std::cout << " .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------." << std::endl;
+	std::cout << "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |" << std::endl;
+	std::cout << "| |  ___  ____   | || |  _________   | || |   _____      | || |     __O__    | || | ____    ____ | || |  _________   | |" << std::endl;
+	std::cout << "| | |_  ||_  _|  | || | |_   ___  |  | || |  |_   _|     | || |    |_   _|   | || ||_   \\  /   _|| || | |_   ___  |  | |" << std::endl;
+	std::cout << "| |   | |_/ /    | || |   | |_  \\_|  | || |    | |       | || |      | |     | || |  |   \\/   |  | || |   | |_  \\_|  | |" << std::endl;
+	std::cout << "| |   |  __'.    | || |   |  _|  _   | || |    | |   _   | || |      | |     | || |  | |\\  /| |  | || |   |  _|  _   | |" << std::endl;
+	std::cout << "| |  _| |  \\ \\_  | || |  _| |___/ |  | || |   _| |__/ |  | || |     _| |_    | || | _| |_\\/_| |_ | || |  _| |___/ |  | |" << std::endl;
+	std::cout << "| | |____||____| | || | |_________|  | || |  |________|  | || |    |_____|   | || ||_____||_____|| || | |_________|  | |" << std::endl;
+	std::cout << "| |              | || |              | || |              | || |              | || |              | || |              | |" << std::endl;
+	std::cout << "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |" << std::endl;
+	std::cout << " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'" << std::endl;
+	std::cout << "          .----------------.  .----------------.  .----------------.  .-----------------. .----------------. " << std::endl;
+	std::cout << "         | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |" << std::endl;
+	std::cout << "         | |     ____     | || |  ____  ____  | || | _____  _____ | || | ____  _____  | || | _____  _____ | |" << std::endl;
+	std::cout << "         | |   .'    `.   | || | |_  _||_  _| | || ||_   _||_   _|| || ||_   \\|_   _| | || ||_   _||_   _|| |" << std::endl;
+	std::cout << "         | |  /  .--.  \\  | || |   \\ \\  / /   | || |  | |    | |  | || |  |   \\ | |   | || |  | |    | |  | |" << std::endl;
+	std::cout << "         | |  | |    | |  | || |    \\ \\/ /    | || |  | '    ' |  | || |  | |\\ \\| |   | || |  | '    ' |  | |" << std::endl;
+	std::cout << "         | |  \\  `--'  /  | || |    _|  |_    | || |   \\ `--' /   | || | _| |_\\   |_  | || |   \\ `--' /   | |" << std::endl;
+	std::cout << "         | |   `.____.'   | || |   |______|   | || |    `.__.'    | || ||_____|\\____| | || |    `.__.'    | |" << std::endl;
+	std::cout << "         | |              | || |              | || |              | || |              | || |              | |" << std::endl;
+	std::cout << "         | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |" << std::endl;
+	std::cout << "          '----------------'  '----------------'  '----------------'  '----------------'  '----------------' " << std::endl;
+	std::cout << "\n\n\n\nKelime Oyunu'na hos geldiniz!";
+	std::cout << "\n\n\nKurallar:";
+	std::cout << "\n\n-> " << HARF_BASI_SORU_S << "'er taneden ";
+	const unsigned short soru_no = HARF_BASI_SORU_S * (MAX_HARF_SAYISI - MIN_HARF_SAYISI + 1);
+	unsigned short toplam_harf = 0;
+	for (unsigned short i = MIN_HARF_SAYISI; i <= MAX_HARF_SAYISI; i++) {
+		toplam_harf += i * HARF_BASI_SORU_S;
+		std::cout << i << (i == MAX_HARF_SAYISI - 1 ? " ve " : ", ");
+	}
+	std::cout << "\b\b harfli; toplam " << soru_no << " kelime ya da kelime grubunu verilen tanimlara gore ";
+	if (OYUN_SURESI / 60 > 0)
+		std::cout << OYUN_SURESI / 60 << " dakika ";
+	if (OYUN_SURESI % 60 > 0)
+		std::cout << OYUN_SURESI % 60 << " saniye ";
+	std::cout << "icersinde bilmeye calisacaksiniz.";
+	std::cout << "\n\n-> Bu " << soru_no << " kelimeden (" << toplam_harf << " harf) bildiginiz her harf icin " << HARF_BASI_PUAN << " puan kazanacaksiniz.";
+	std::cout << "\n\n-> Yardim icin '" << HARF_ALAYIM << "' tusuna basarak harf alabilir ama aldiginiz harflerden puan alamazsiniz, sureyi '";
+	std::cout << TAHMIN << "' tusuna (Tahmin) basarak durdurabilir ama durdurduktan sonra harf alamazsiniz.";
+	std::cout << "\n\n-> Sureyi durdurduktan itibaren kelimeyi bilmeniz icin";
+	if (TAHMIN_SURESI / 60 > 0)
+		std::cout << " " << TAHMIN_SURESI / 60 << " dakika";
+	TAHMIN_SURESI % 60 > 0 ? std::cout << " " << TAHMIN_SURESI % 60 << " saniyeniz" : std::cout << "niz";
+	std::cout << " olacak, bu sure icinde kelimeyi bilemezseniz bilemediginiz her harf icin " << HARF_BASI_PUAN << " puan kaybedeceksiniz.";
+	std::cout << "\n\n-> Oyunu istediginiz zaman durdurmak icin '" << DURDURMA_TUSU << "' tusuna, cikmak icin de 'Escape' tusuna basabilirsiniz.";
+	std::cout << "\n\n\Dilediginiz herhangi bir tusuna basarak oyunu baslatabilirsiniz.";
+	if (_getch() == CIKIS_TUSU)
+		exit(0);
+	system("cls");
+}
+
+
+void KelimeOyunu::PrintLoadingScreen() {
+	ImlecTasiXY(40, 10);
+	std::cout << "Yukleniyor...";
+	ImlecTasiXY(34, 11);
+	for (unsigned short i = 0; i < 20; i++) {
+		Sleep(50);
+		std::cout << char(182);
+	}
+	system("cls");
+}
